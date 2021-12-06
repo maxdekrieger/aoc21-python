4998df45cf9df71e40d10ac71607c23eed91e90a
Max de Krieger <maxdekrieger@gmail.com>
2021-12-06 14:21:58 +0100

    revise day 5 part 1

--- day5/part1/main.py
+++ day5/part1/main.py
@@ -1,5 +1,5 @@
 def main():
-    with open('../data/input-test.txt', 'r') as file:
+    with open('../data/input.txt', 'r') as file:
 
         vents: list[list[int]] = [[0 for y in range(999)] for x in range(999)]
 
@@ -10,20 +10,22 @@ def main():
             x2 = int(line[2])
             y2 = int(line[3])
 
+            if x1 > x2:
+                temp = x1
+                x1 = x2
+                x2 = temp
+
+            if y1 > y2:
+                temp = y1
+                y1 = y2
+                y2 = temp
+
             if x1 == x2:
-                if y2 >= y1:
-                    for i in range(y2 - y1 + 1):
-                        vents[x1][y1 + i] += 1
-                else:
-                    for i in range(y1 - y2 + 1):
-                        vents[x1][y2 + i] += 1
+                for i in range(y2 - y1 + 1):
+                    vents[x1][y1 + i] += 1
             elif y1 == y2:
-                if x2 >= x1:
-                    for i in range(x2 - x1 + 1):
-                        vents[x1 + i][y1] += 1
-                else:
-                    for i in range(x1 - x2 + 1):
-                        vents[x2 + i][y1] += 1
+                for i in range(x2 - x1 + 1):
+                    vents[x1 + i][y1] += 1
 
         double_vent_amount = 0
         for column in vents:
