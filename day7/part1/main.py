806a762ab6005ef403e9031364bc4ef28feed485
Max de Krieger <maxdekrieger@gmail.com>
2021-12-07 09:42:27 +0100

    day 7 part 1 complete

---
+++ day7/part1/main.py
@@ -0,0 +1,21 @@
+from sys import maxsize
+
+def main():
+    with open('../data/input.txt', 'r') as file:
+
+        original_positions = [int(s) for s in file.readline().strip().split(',')]
+        highest = max(original_positions)
+        lowest = min(original_positions)
+
+        lowest_fuel_cost = maxsize
+        for target in range(lowest, highest + 1):
+            fuel = 0
+            for pos in original_positions:
+                required = (target - pos) if target > pos else (pos - target)
+                fuel += required
+            lowest_fuel_cost = fuel if fuel < lowest_fuel_cost else lowest_fuel_cost 
+
+        print(lowest_fuel_cost)
+
+if __name__ == '__main__':
+    main()
